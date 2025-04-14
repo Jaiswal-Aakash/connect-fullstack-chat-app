23ed9a8ab2024b0dce70e04635231d26b98148b1
Akash Jaiswal <realakashjaiswal@gmail.com>
2025-04-14 11:37:27 +0530

    fixing jwt import

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -7,7 +7,7 @@ export const protectRoute = async (req, res, next) => {
         if(!token) {
             return res.status(401).json({ message: "Unauthorized - No Token Provided" });
         }
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const decoded = jwt.verify(token, process.env.JWt_SECRET);
             if(!decoded) {
                 return res.status(401).json({ message: "Unauthorized - Invalid Token" });
             }
