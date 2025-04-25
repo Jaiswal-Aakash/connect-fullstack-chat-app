f16e153b45df1557295f3bca802a2edebb07f26d
Akash Jaiswal <realakashjaiswal@gmail.com>
2025-04-26 00:47:24 +0530

    made login page responsive

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -20,7 +20,7 @@ const SignupPage = ({ onToggle }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen w-full relative bg-white overflow-hidden">
+    <div className="min-h-[100dhv] w-full relative bg-white overflow-hidden">
       <div
         className="absolute top-0 left-[-80px] w-[850px] h-[500px] rounded-full mix-blend-multiply filter blur-2xl opacity-80 -translate-y-2/4"
         style={{ backgroundColor: "rgba(170, 252, 255, 1)" }}
@@ -31,10 +31,10 @@ const SignupPage = ({ onToggle }) => {
         style={{ backgroundColor: "rgba(159, 188, 255, 1)" }}
       ></div>
 
-      <div className="container mx-auto min-h-screen flex items-center justify-center px-4">
+      <div className="container mx-auto min-h-screen flex items-center justify-center px-4 py-0">
         <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           {/* Left Side - Branding */}
-          <div className="flex flex-col md:flex-row items-center justify-center text-center space-y-4 md:space-y-0 md:space-x-6 w-full max-w-[40rem] mx-auto z-10 h-screen">
+          <div className="flex flex-col md:flex-row items-center justify-center text-center space-y-4 md:space-y-0 md:space-x-6 w-full max-w-[40rem] mx-auto z-10 md:h-screen">
             <img
               src="./chatIcon.png"
               className="w-[80px] sm:w-[100px] md:w-[100px] max-w-full m-3 md:m-1"
@@ -55,7 +55,7 @@ const SignupPage = ({ onToggle }) => {
           </div>
 
           {/* Right Side - Auth Form */}
-          <div className="bg-white p-8 rounded-2xl shadow-xl backdrop-blur-sm bg-opacity-80">
+          <div className="bg-white p-8 rounded-2xl shadow-xl backdrop-blur-sm bg-opacity-80 overflow-y-auto max-h-[90vh]">
             <form onSubmit={handleSubmit} className="space-y-6">
               <h2 className="text-3xl font-bold text-gray-900">Login</h2>
 
